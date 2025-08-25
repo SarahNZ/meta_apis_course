6be277ef192c22d3c61fab3611a5959ffe062f34
Sarah Stenfert <sarah.stenfert@gmail.com>
2025-08-25 16:00:06 +1200

    Moved welcome and menu function-based views to the bottom of the views.py file, as I'm currently working exclusively on the menu_items view. This view is now able to receive querystring parameters so the results can be filtered or sorted.

--- projects/LittleLemon/LittleLemonAPI/views.py
+++ projects/LittleLemon/LittleLemonAPI/views.py
@@ -10,23 +10,28 @@ from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.renderers import StaticHTMLRenderer
 from rest_framework.decorators import api_view, renderer_classes
 
-@api_view()
-@renderer_classes([StaticHTMLRenderer])
-def welcome(request):
-    data = '<html><body><h1>Welcome to Little Lemon API Project</h1></body></html>'
-    return Response(data)
-
-@api_view()
-@renderer_classes([TemplateHTMLRenderer])
-def menu(request):
-    items = MenuItem.objects.select_related('category').all()
-    serialized_item = MenuItemSerializer(items, many = True)
-    return Response({'data': serialized_item.data}, template_name = 'menu-items.html')
-
 @api_view(['GET', 'POST'])
 def menu_items(request):
     if request.method == 'GET':
         items = MenuItem.objects.select_related('category').all()
+        category_name = request.query_params.get('category')
+        to_price = request.query_params.get('to_price')
+        search = request.query_params.get('search')
+        ordering = request.query_params.get('ordering')
+        
+        if category_name:
+            items = items.filter(category__title__iexact = category_name)
+        if to_price:
+            items = items.filter(price__lte = to_price) # lte is less than or equal to dunder instance method
+            # items = items.filter(price = to_price)
+        if search:
+            items = items.filter(title__icontains = search)
+            # items = items.filter(title__istartswith = search)
+        if ordering:
+            # items = items.order_by(ordering)
+            ordering_fields = ordering.split(",")
+            items = items.order_by(*ordering_fields)
+            
         serialized_item = MenuItemSerializer(items, many = True)
         return Response(serialized_item.data)
     if request.method == 'POST':
@@ -54,4 +59,18 @@ class MenuItemsView(generics.ListCreateAPIView):
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+    
+@api_view()
+@renderer_classes([StaticHTMLRenderer])
+def welcome(request):
+    data = '<html><body><h1>Welcome to Little Lemon API Project</h1></body></html>'
+    return Response(data)
+
+@api_view()
+@renderer_classes([TemplateHTMLRenderer])
+def menu(request):
+    items = MenuItem.objects.select_related('category').all()
+    serialized_item = MenuItemSerializer(items, many = True)
+    return Response({'data': serialized_item.data}, template_name = 'menu-items.html')
+
     
