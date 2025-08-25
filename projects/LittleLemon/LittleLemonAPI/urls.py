f2a1f67eb4b98af9cf63812b93ef55d0b5dd7e19
Sarah Stenfert <sarah.stenfert@gmail.com>
2025-08-25 15:56:01 +1200

    Added a forward slash to the path for the menu-items endpoint so I could use query parameters for filtering and ordering.

--- projects/LittleLemon/LittleLemonAPI/urls.py
+++ projects/LittleLemon/LittleLemonAPI/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('menu-items', views.menu_items),
+    path('menu-items/', views.menu_items),
     path('menu-items/<int:id>', views.single_item),
     path('category/<int:pk>/', views.category_detail, name = 'category-detail'),
     path('menu', views.menu),
