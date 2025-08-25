f7707f7695d04bd6711ae36a5266f9e268387de4
Sarah Stenfert <sarah.stenfert@gmail.com>
2025-08-25 17:20:14 +1200

    Added a few different ways of validating data in the MenuItemSerializer class

--- projects/LittleLemon/LittleLemonAPI/serializers.py
+++ projects/LittleLemon/LittleLemonAPI/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from rest_framework.validators import UniqueTogetherValidator
 from .models import MenuItem
 from .models import Category
 from decimal import Decimal, ROUND_HALF_UP
-
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,10 +15,40 @@ class MenuItemSerializer(serializers.ModelSerializer):
     price_after_tax = serializers.SerializerMethodField(method_name = 'calculate_tax')
     category = CategorySerializer(read_only = True)
     category_id = serializers.IntegerField(write_only = True)
+    title = serializers.CharField(
+        max_length = 255,
+        validators = [UniqueValidator(queryset = MenuItem.objects.all())]
+    )
+    # price = serializers.DecimalField(max_digits = 6, decimal_places = 2, min_value = 2)
+    
+    # def validate_price(self, value):
+    #     if value < 2:
+    #         raise serializers.ValidationError('Price should not be less than 2.0')
+    #     return value # Return value if it passes validation
+    
+    # def validate_stock(self, value):
+    #     if value < 0:
+    #         raise serializers.ValidationError('Stock cannot be negative')
+    #     return value # Return value if it passes valication
+    
+    def validate(self, attrs):
+        if(attrs['price'] < 2):
+            raise serializers.ValidationError('Price should not be less than 2.0')
+        if(attrs['inventory'] < 0):
+            raise serializers.ValidationError('Stock cannot be negative')
+        return super().validate(attrs)
     
     class Meta:
         model = MenuItem
         fields = ['id', 'title', 'price', 'price_after_tax', 'stock', 'category', 'category_id']
+        extra_kwargs = {
+            'price': {'min_value': 2},
+            # 'title': {
+            #     'validators': [
+            #         UniqueValidator(queryset = MenuItem.objects.all())
+            #     ]
+            # }
+        }
         
     def calculate_tax(self, product:MenuItem):
         price_with_tax = product.price * Decimal("1.1")
