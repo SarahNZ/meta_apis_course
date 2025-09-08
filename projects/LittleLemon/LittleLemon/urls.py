b2cfbb0d65da55b8b7e695c27de775ee3d6fc38a
Sarah Stenfert <sarah.stenfert@gmail.com>
2025-09-08 15:46:30 +1200

    Updated steps and progress made in technical plan. Also updated test user data. Added ability of managers to add and remove users from the Manager group.

--- projects/LittleLemon/LittleLemon/urls.py
+++ projects/LittleLemon/LittleLemon/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
-    # path('api/', include('LittleLemonAPI.urls')),
+    path('api/', include('LittleLemonAPI.urls')),
 ]
--- projects/LittleLemon/LittleLemonAPI/views.py
+++ projects/LittleLemon/LittleLemonAPI/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User, Group
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+# Managers can view a list of all the users in the Manager group GET /api/groups/manager/users
+@api_view(['POST', 'DELETE'])
+@permission_classes([IsAdminUser])
+def managers(request):
+        username = request.data['username']
+        if username:
+            user = get_object_or_404(User, username = username)
+            managers = Group.objects.get(name = 'Manager')
+            if request.method == 'POST':
+                managers.user_set.add(user)
+                return Response({"message": "ok"}, status.HTTP_201_CREATED)
+            elif request.method == 'DELETE':
+                managers.user_set.remove(user)
+                return Response({"message": "ok"}, status.HTTP_200_OK)
+
+        return Response({"message": "error"}, status.HTTP_400_BAD_REQUEST)
